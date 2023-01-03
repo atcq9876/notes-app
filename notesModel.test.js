b3e1e84cb442be11b515d9cd5f1d1325630ea6a5
Andy <ajscottlewis@gmail.com>
2023-01-03 13:25:55 +0000

    Test and implement NotesModel class

---
+++ notesModel.test.js
@@ -0,0 +1,26 @@
+const NotesModel = require('./notesModel');
+
+describe('NotesModel', () => {
+  it('returns list of all notes', () => {
+    const notes = new NotesModel;
+
+    expect(notes.getNotes()).toEqual([]);
+  })
+
+  it('adds notes to list', () => {
+    const notes = new NotesModel;
+    notes.addNote('Buy milk');
+    notes.addNote('Go to the gym');
+
+    expect(notes.getNotes()).toEqual(['Buy milk', 'Go to the gym']);
+  })
+
+  it('clears all notes from the list', () => {
+    const notes = new NotesModel;
+    notes.addNote('Buy milk');
+    notes.addNote('Go to the gym');
+    notes.reset();
+
+    expect(notes.getNotes()).toEqual([]);    
+  })
+})
